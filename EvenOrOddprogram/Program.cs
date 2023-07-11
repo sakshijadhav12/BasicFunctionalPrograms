299c33bae735e83515d2f518a754b0fb78ace530
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-11 23:18:53 +0530

    [sakshi] uploding even or odd program

---
+++ EvenOrOddprogram/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EvenOrOddprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number that you want to check");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("Given number is even");
+            else
+                Console.WriteLine(" Givennumber is odd ");
+        }
+    }
+}
